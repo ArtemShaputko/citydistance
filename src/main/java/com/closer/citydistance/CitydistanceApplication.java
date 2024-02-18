a1914042853ddcd88a3556c2af1d33d7c02a7d6d
Artem Shaputsko <artem.shaputko@gmail.com>
2024-02-19 02:26:58 +0300

    First commit

---
+++ src/main/java/com/closer/citydistance/CitydistanceApplication.java
@@ -0,0 +1,13 @@
+package com.closer.citydistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitydistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitydistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/closer/citydistance/CitydistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closer.citydistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitydistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
